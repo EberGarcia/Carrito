d285f1785f3e35c051c6d21e3197d7a1acc7103c
EberGarcia <garciaayazanaeber@gmail.com>
2021-05-19 22:46:37 -0500

    agregar carrito JS

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
 
 // Crear eventos
 cargarEventos();
@@ -11,6 +12,7 @@ function cargarEventos() {
 
 // Agregar curso
 function cargarEvento(e) {
+	e.preventDefault();
 	if (e.target.classList.contains('agregar-carrito')) {
 		const cursoSeleccionado = e.target.parentElement.parentElement;
 		leerCurso(cursoSeleccionado);
@@ -19,5 +21,48 @@ function cargarEvento(e) {
 
 // Leer curso seleccionado
 function leerCurso(curso) {
-	console.log(curso);
+	const infoCurso = {
+		img: curso.querySelector('img').src,
+		titulo: curso.querySelector('h4').textContent,
+		precio: curso.querySelector('.precio span').textContent,
+		id: curso.querySelector('a').getAttribute('data-id'),
+		cantidad: 1
+	};
+	console.log(infoCurso);
+
+	articulosCarrito = [ ...articulosCarrito, infoCurso ];
+
+	carritoHTML();
+}
+
+// Mostrar en conetenedor HTML
+function carritoHTML() {
+	// Limpiar HTML
+	limpiarHTML();
+
+	// Recorrer array
+	articulosCarrito.forEach((curso) => {
+		const row = document.createElement('tr');
+		row.innerHTML = `
+		<td>
+			<img src="${curso.img}" width="100">
+		</td>
+		<td> ${curso.titulo} </td>
+		<td> ${curso.precio} </td>
+		<td> ${curso.cantidad} </td>
+		<td>
+			<a href="#" class="borrar-curso">X</a>
+		</td>
+		`;
+
+		// Mostrar en HTML
+		contenedorCarrito.appendChild(row);
+	});
+}
+
+// Limpiar HTML
+function limpiarHTML() {
+	while (contenedorCarrito.firstChild) {
+		contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+	}
 }
